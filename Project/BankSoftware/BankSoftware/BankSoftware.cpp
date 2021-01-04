83ef6eb2288bce8a507f4c551966fdf0069a9a1e
Yusmen <mojito@abv.bg>
2021-01-04 09:24:37 +0200

    Initial Commit

---
+++ Project/BankSoftware/BankSoftware/BankSoftware.cpp
@@ -0,0 +1,58 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+struct User
+{
+	char username[30];
+	char password[20];
+	double balance;
+
+};
+char MainMenu();
+void Processing();
+int main()
+{
+
+	Processing();
+}
+void Processing()
+{
+	char username[30];
+	char password[20];
+	char option = MainMenu();
+	if (option == 'L')
+	{
+		cout << "Enter Name:  "; cin >> username;
+		cout << endl;
+		cout << "Enter Password:  "; cin >> password;
+	}
+	else if (option == 'R')
+	{
+		cout << "Enter Name:  "; cin >> username;
+		cout << endl;
+		cout << "Enter Password:  "; cin >> password;
+	}
+	else 
+	{
+		exit(0);
+	}
+}
+
+char MainMenu()
+{
+	char input;
+	cout << endl;
+	cout << "     Choose one of the options:   " << endl;
+	cout << endl;
+	cout << endl;
+	cout << "L for login" << "    " << "R for register" << "    ." << "Q for quit";
+	cout << endl;
+	cin >> input;
+
+	return input;
+}
+
+
+
